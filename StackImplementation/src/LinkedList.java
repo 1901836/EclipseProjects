efcba6cea084b42130a169ec8f5b89536bb4915c
1901836 <1901836@00-46236.pusd.dom>
2020-02-12 10:20:57 -0800

    :)

--- StackImplementation/src/LinkedList.java
+++ StackImplementation/src/LinkedList.java
@@ -9,7 +9,8 @@ public class LinkedList<T>{ //<T> allows us to use any data type
 	
 	public LinkedList(){
 		//job of the constructor is to initialize the instance variables
-		
+		size = size;
+		head  = head;
 		
 	}
 	
@@ -38,37 +39,56 @@ public class LinkedList<T>{ //<T> allows us to use any data type
 	}
 	public void addFront(T t){ //add to the beginning of the list
 	size++;
+	Node temp = head;
 	if(head==null){
 		Node n = new Node(t); //creating another new node that encapsulates the data t
 		head = n; //head reference now points to this first node
 		return;
 	}
-	
+	temp = temp.next();
+	temp = new Node(t);
 	
 	
 	
 	}
 	public Node remove(){ //remove and returns first element(Head)
 		
+	Node temp = head;
 		if(head==null){
 			return null;
 		}
 		
+		
+		head = temp.next();
 		size--;
-		head = null;
-		return head;
-	
+		return temp;
+		
 	}
 	/* remove node at index i
 	 * REQUIRES i < size
 	 */
 	public Node remove(int i){ // remove and returns element at the given index
-		size--;
-		if(i < size){
-		Node temp = head;
-			
+		if(i > size){
+			return null;
 		}
-		return null;
+		if(head == null){
+			return null;
+		}
+		
+		Node temp = head;
+		if(i==0){
+			head = null;
+			size--;
+			return null;}
+		
+		if(i<size){
+			temp = temp.next();
+		size--;
+		}
+			
+			
+		return temp;
+		
 	}
 	public boolean isCircular(){
 		return false;
@@ -76,6 +96,9 @@ public class LinkedList<T>{ //<T> allows us to use any data type
 	
 	public int size(){ //returns number of elements
 		return size;
+	}
+	public String toString(){
+		return head.toString();
 	}
 	
 }
